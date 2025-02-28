2ad0988defadc77a6241472c354e585cb31ce3b8
jaystile <142552281+pico-bolero@users.noreply.github.com>
2025-02-28 06:25:26 -0500

    documents and extracts none-ls

--- lua/custom/plugins/autosession.lua
+++ lua/custom/plugins/autosession.lua
@@ -1,5 +1,7 @@
+-- https://github.com/rmagatti/auto-session
 -- autosession should save a session file in the directory and restore the set
--- of files that you were working on.
+-- of files that you were working on. This was brought in to allow exiting and
+-- entering a project and restoring where you left off with regards to open files.
 return {
   'rmagatti/auto-session',
   lazy = false,
